b3627f600bb226656f029570a3f9364d5e6fe316
NajherySoler <najhery_02@hotmail.com>
2022-01-31 19:45:29 -0500

    carpeta de bluej

---
+++ segundo/CategoriaCredencial.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class CategoriaCredencial here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CategoriaCredencial
+{
+    // instance variables - replace the example below with your own
+    private String nombre;
+    private String tipo;
+    
+
+    /**
+     * Constructor for objects of class CategoriaCredencial
+     */
+    public CategoriaCredencial()
+    {
+        // initialise instance variables
+       
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    
+}
---
+++ segundo/Usuario.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Usuario here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Usuario
+{
+    // instance variables - replace the example below with your own
+    private String correo;
+    private String contraseña;
+    private String verificarcontraseña;
+    private String rol;
+    private Credencial credencial;
+
+    /**
+     * Constructor for objects of class Usuario
+     */
+    public Usuario()
+    {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+   
+}
